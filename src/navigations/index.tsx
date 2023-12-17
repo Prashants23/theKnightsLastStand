7c32594a702806327f75838ac60cef947d4a6880
Prashant-betterhalf <prashant@betterhalf.ai>
2023-12-18 00:04:52 +0530

    feat: basic navigation setup

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from "react-native";
-import React from "react";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "~screens/index";
 
-export default function index() {
+const Stack = createNativeStackNavigator();
+
+export const Navigator = () => {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Welcome" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
